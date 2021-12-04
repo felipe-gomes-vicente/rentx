55b9283e467853c253965093acc2a58e5a9a767b
Felipe Gomes Vicente <felipegomes.vicente2@gmail.com>
2021-12-04 12:22:33 -0300

    criação card car

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -3,9 +3,30 @@ import { StatusBar } from "react-native";
 import { RFValue } from "react-native-responsive-fontsize";
 
 import Logo from "../../assets/logo.svg";
+import { Car } from "../../components/Car";
 import { Container, Header, HeaderContent, TotalCars } from "./styles";
 
 export function Home(){
+  const carDataOne = {
+    brand: 'Audi',
+    name: 'R$ 5 Coupé',
+    rent: {
+    period: 'Ao dia',
+    price: 220,
+    },
+    thumbnail: 'https://pngimg.com/uploads/audi/audi_PNG99491.png'
+  }
+
+  const carDataTwo = {
+    brand: 'Porsche',
+    name: 'Panamera',
+    rent: {
+    period: 'Ao dia',
+    price: 300,
+    },
+    thumbnail: 'https://cdn.picpng.com/porsche/porsche-picture-29281.png'
+  }
+
   return (
     <Container>
       <StatusBar 
@@ -22,7 +43,8 @@ export function Home(){
         </HeaderContent>
       </Header>
 
-      
+      <Car data={carDataOne} />
+      <Car data={carDataTwo} />
     </Container>
   )
 }
