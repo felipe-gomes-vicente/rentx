38869f17964bbc40510fd6c4fc81c7eb8a053d93
Felipe Gomes Vicente <felipegomes.vicente2@gmail.com>
2021-12-04 11:12:39 -0300

    configurar arquivo svg e png

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,13 +1,28 @@
 import React from "react";
+import { StatusBar } from "react-native";
+import { RFValue } from "react-native-responsive-fontsize";
 
-import { Container, Header } from "./styles";
+import Logo from "../../assets/logo.svg";
+import { Container, Header, HeaderContent, TotalCars } from "./styles";
 
 export function Home(){
   return (
     <Container>
+      <StatusBar 
+        barStyle="light-content" 
+        backgroundColor="transparent"
+        translucent
+      />
       <Header>
-        HOME
+        <HeaderContent>
+          <Logo width={RFValue(108)} height={RFValue(12)} />
+          <TotalCars>
+          Total de 12 carros
+          </TotalCars>
+        </HeaderContent>
       </Header>
+
+      
     </Container>
   )
 }
