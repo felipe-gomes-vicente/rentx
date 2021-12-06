375a3836ed3bad134ddea87faa8ff9e61e705ade
Felipe Gomes Vicente <felipegomes.vicente2@gmail.com>
2021-12-06 20:40:41 -0300

    interface Scheduling Complete

--- App.tsx
+++ App.tsx
@@ -5,7 +5,7 @@ import { useFonts, Inter_400Regular, Inter_500Medium } from "@expo-google-fonts/
 import { Archivo_400Regular, Archivo_500Medium, Archivo_600SemiBold } from "@expo-google-fonts/archivo";
 
 import theme from "./src/styles/theme";
-import { SchedulingDetails } from "./src/screens/SchedulingDetails";
+import { SchedulingComplete } from "./src/screens/SchedulingComplete";
 
 export default function App() {
   const [fontsLoaded] = useFonts({
@@ -23,7 +23,7 @@ export default function App() {
   return (
     <ThemeProvider theme={theme}>
       
-      <SchedulingDetails />
+      <SchedulingComplete />
     </ThemeProvider>
     
   );
