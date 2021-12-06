823a0c69bc7d6ae576e39dfcbbc5ed1739f632ff
Felipe Gomes Vicente <felipegomes.vicente2@gmail.com>
2021-12-05 23:15:16 -0300

    interface de detalhes

--- src/screens/CarDetails/index.tsx
+++ src/screens/CarDetails/index.tsx
@@ -3,18 +3,54 @@ import React from "react";
 import { BackButton } from "../../BackButton";
 import { ImageSlider } from "../../components/imageSlider";
 
-import { Container, Header, CarImages } from "./styles"
+import {
+  Container,
+  Header,
+  CarImages,
+  Content,
+  Details,
+  Description,
+  Brand,
+  Name,
+  Rent,
+  Period,
+  Price,
+  About,
+} from "./styles";
 
-export function CarDetails(){
-  return(
+export function CarDetails() {
+  return (
     <Container>
       <Header>
-        <BackButton onPress={() => {}}/>        
+        <BackButton onPress={() => {}} />
       </Header>
 
       <CarImages>
-        <ImageSlider imageUrl={['https://pngimg.com/uploads/audi/audi_PNG99491.png']} />
+        <ImageSlider
+          imageUrl={["https://pngimg.com/uploads/audi/audi_PNG99491.png"]}
+        />
       </CarImages>
+
+      <Content>
+        <Details>
+          <Description>
+            <Brand>Lamborguini</Brand>
+            <Name>Huracan</Name>
+          </Description>
+
+          <Rent>
+            <Period>Ao Dia</Period>
+            <Price>R$ 500</Price>
+          </Rent>
+        </Details>
+
+        <About>
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Deleniti
+          nostrum autem rem quidem, provident voluptas iusto eveniet nam vel,
+          excepturi similique ipsam. Nesciunt et eaque dolorum soluta assumenda,
+          cumque repellendus!
+        </About>
+      </Content>
     </Container>
-  )
+  );
 }
