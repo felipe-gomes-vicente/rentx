b4068d6517db2dfe4b17b7c4ecd794caab7c3660
Felipe Gomes Vicente <felipegomes.vicente2@gmail.com>
2021-12-06 10:20:53 -0300

    interface Scheduling

--- App.tsx
+++ App.tsx
@@ -5,7 +5,7 @@ import { useFonts, Inter_400Regular, Inter_500Medium } from "@expo-google-fonts/
 import { Archivo_400Regular, Archivo_500Medium, Archivo_600SemiBold } from "@expo-google-fonts/archivo";
 
 import theme from "./src/styles/theme";
-import { CarDetails } from "./src/screens/CarDetails";
+import { Scheduling } from "./src/screens/Scheduling";
 
 export default function App() {
   const [fontsLoaded] = useFonts({
@@ -22,7 +22,7 @@ export default function App() {
 
   return (
     <ThemeProvider theme={theme}>
-      <CarDetails />
+      <Scheduling />
     </ThemeProvider>
     
   );
--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -10,7 +10,7 @@ interface Props {
 
 export function Button({title, color, ...rest}: Props){
   return(
-    <Container {...rest}>
+    <Container {...rest} color={color}>
       <Title>{title}</Title>
     </Container>
   )
